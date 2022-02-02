4e3331d21a74debb4947f6e0ec67f592fd7d391c
ndr.singh <ndrsingh487@gmail.com>
2022-02-03 01:08:23 +0530

    add vnet and web subnet tf file

---
+++ c6-03-web-subnet-and-nsg.tf
@@ -0,0 +1,45 @@
+#Resource-1: Create Web Subnet
+resource "azurerm_subnet" "websubnet" {
+    name = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
+    resource_group_name = azurerm_resource_group.rg.name
+    address_prefixes = var.web_subnet_address
+    virtual_network_name = azurerm_virtual_network.vnet.name
+}
+
+#Resource-2: Create Network Security Group (NSG)
+resource "azurerm_network_security_group" "web_subnet_nsg" {
+    name = "${azurerm_subnet.websubnet.name}-nsg"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+}
+
+#Resource-3: Association NSG and Subnet
+resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_association" {
+    subnet_id = azurerm_subnet.websubnet.id
+    network_security_group_id = azurerm_network_security_group.web_subnet_nsg.id
+}
+
+# Resource-4: Create NSG Rules
+## Locals Block for Security Rules
+locals {
+    web_inbound_ports_map = {
+        "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+        "110" : "443",
+        "120" : "22"
+    } 
+}
+    ## NSG Inbound Rule for WebTier Subnets
+resource "azurerm_network_security_rule" "web_nsg_rule_inbound" {
+    for_each = local.web_inbound_ports_map
+    name                        = "Rule-Port-${each.value}"
+    priority                    = each.key
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "*"
+    destination_port_range      = each.value 
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    resource_group_name         = azurerm_resource_group.rg.name
+    network_security_group_name = azurerm_network_security_group.web_subnet_nsg.name
+}
